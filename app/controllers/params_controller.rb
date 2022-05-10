969c1faf3d1271dee81a05649f07a0662fae2bfd
Kevin Zhong <kvnzhong@gmail.com>
2022-05-09 20:46:55 -0700

    Conceding defeat; cannot figure out how to directly pull query string without saving/pulling the thing from a database. Will return later. URL segment support is kosher.

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,5 +1,10 @@
 class ParamsController < ApplicationController
-  def show
+  def show_query
+    phrase = params[:phrase]
+    render json: { phrase: phrase }
+  end
+
+  def show_segment
     phrase = params[:phrase]
     render json: { phrase: phrase }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/:phrase", controller: "params", action: "show"
+  get "/query/:phrase", controller: "params", action: "show_query"
+  get "/segment/:phrase", controller: "params", action: "show_segment"
 end
